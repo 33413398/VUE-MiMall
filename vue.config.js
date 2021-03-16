278a7a48549f56e9bc7e75a13a1e1d9bb14930be
haowei <haowei_web@163.com>
2021-03-16 23:26:36 +0800

    完成购物车开发

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,14 @@ module.exports = {
       }
     }
   },
+  // 部署应用时的基本 URL
+  // publicPath: './',
+  // build时构建文件的目录 构建时传入 --no-clean 可关闭该行为
+  // outputDir: 'dist',
+  // build时放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  // assetsDir: 'static',
+  // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
+  // indexPath: 'index.html',
   // publicPath:'/app',
   // outputDir:'dist',
   // indexPath:'index2.html',
